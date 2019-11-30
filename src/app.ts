81be4189ec3c84df5dc0fea79f5b7e80a23be8cb
Nance <nance.kenneth@mayo.edu>
2019-11-30 14:59:03 -0600

    Adding ui to get it working

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import { uiH2, renderControlElement } from "./ui/elements/createelements";
-import { headerMainLabel, searchTextDef } from "./ui/header";
-import { converge } from "ramda"
-
-const div2 = document.getElementById('div2') as HTMLDivElement;
 
 
-div2.appendChild(renderControlElement(searchTextDef))
+const searchInputElement = document.getElementById('clubsearchinput') as HTMLButtonElement;
+const searchInputButton = document.getElementById('clubsearchbutton') as HTMLButtonElement;
 
+searchInputButton.addEventListener('click',() => {
+    if(searchInputElement == null) return;
+    const searchTerm = searchInputElement.value;
+    console.log("SearchTerm:",searchTerm)
+})
